d0244eb621b45121e4fefb6bb91db6b8b256b64b
hmd123456 <95592211+hmd123456@users.noreply.github.com>
2023-04-18 09:29:26 +0100

    Add files via upload

--- pro.tf
+++ pro.tf
@@ -3,7 +3,7 @@ terraform {
     organization = "mmd-dev"
 
     workspaces {
-      name = "mmddevws"
+      name = "WS01"
     }
   }
 }
--- var.tf
+++ var.tf
@@ -1,11 +1,14 @@
 variable "vpc_cidr_block" {
   description = "CIDR block for VPC"
   type        = string
-  default     = "10.0.1.0/16"
+  default     = "10.0.0.0/16"
   
 }
 
 variable "ENV2" {default=""}
+
+variable "AWS_ACCESS_KEY_ID" {default="AKIA5BBQL4PJG5R5E67D"}
+variable "AWS_SECRET_ACCESS_KEY" {default="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"}
  
 variable "ENV3" {
   description = "env"
