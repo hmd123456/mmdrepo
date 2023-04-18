aa0c8d5bf78341e4f251674667b46ec281aa65de
hmd123456 <95592211+hmd123456@users.noreply.github.com>
2023-04-18 09:31:16 +0100

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,16 +1,11 @@
 provider "aws" {
   
   region  = "eu-west-2"
-<<<<<<< HEAD
+ 
   access_key = "AKIA5BBQL4PJG5R5E67D"
   secret_key = "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"
   #access_key = var.AWS_ACCESS_KEY_ID
   #secret_key = var.AWS_SECRET_ACCESS_KEY
-=======
-  #access_key = "AKIA5BBQL4PJG5R5E67D"
-  #secret_key = "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"
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
->>>>>>> 9607511 (test)
+ 
 
 }
